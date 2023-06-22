350f4eb5c48c5606224624877fc01895d1c0302a
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-06-22 16:46:58 -0700

    Created JS Quiz template

---
+++ script.js
@@ -0,0 +1,60 @@
+var quizArray = [
+    {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    },
+    {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    }
+    ,  {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    },
+  
+
+    {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    },
+
+    {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    },
+
+    {
+        question:"What is HTML?",
+        option1:"HyperDance text",
+        option2:"Hypertext Markup Language",
+        option3:"HyperText Makeup Language",
+        option4:"Hypertext Markup Literature",
+        answer:"Hypertext Markup Language"
+    }
+
+
+
+
+    
+
+]
