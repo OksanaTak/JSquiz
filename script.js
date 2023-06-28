15d79bd6824e3624a37f1fbbfb0f9303032168d0
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-06-27 18:15:03 -0700

    Quiz Questions & Answers

--- script.js
+++ script.js
@@ -1,62 +1,105 @@
+var currentQuestion = 0
+var questionElement = document.getElementById('questions')
+var option1 = document.getElementById('option1')
+var option2 = document.getElementById('option2')
+var option3 = document.getElementById('option3')
+var option4 = document.getElementById('option4')
+var checkAnswer = document.getElementById('checkAnswer')
+
+var score = 0
+option1.addEventListener('click', correctAnswer)
+option2.addEventListener('click', correctAnswer)
+option3.addEventListener('click', correctAnswer)
+option4.addEventListener('click', correctAnswer)
+
 var quizArray = [
-    {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    },
-    {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    }
-    ,  {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    },
-  
+  {
+    question: 'Which of the following are included in arrays in JavaScript',
+    option1: 'numbers and strings',
+    option2: 'booleans',
+    option3: 'other arrays',
+    option4: 'all of the above',
+    answer: 'all of the above'
+  },
+  {
+    question:
+      'Which of the following is NOT a valid way to declare a variable in JavaScript',
+    option1: 'var',
+    option2: 'const',
+    option3: 'let',
+    option4: 'int',
+    answer: 'int'
+  },
+  {
+    question: 'What is the correct way to comment a single-line in JavaScript?',
+    option1: '// comment',
+    option2: '<!-- comment -->',
+    option3: '/* comment */',
+    option4: '** comment **',
+    answer: '** comment **'
+  },
 
-    {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    },
+  {
+    question: 'What is the correct way to declare a function in JavaScript?',
+    option1: 'myFunction()',
+    option2: 'function myFunction()',
+    option3: 'var myFunction = function()',
+    option4: 'All of the above',
+    answer: 'All of the above'
+  },
 
-    {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    },
+  {
+    question: 'What is the correct way to create an object in JavaScript',
+    option1: 'var obj = {}',
+    option2: 'var obj = new Object()',
+    option3: 'Both a and b are correct',
+    option4: 'None of the above',
+    answer: 'Both a and b are correct'
+  },
 
-    {
-        question:"What is HTML?",
-        option1:"HyperDance text",
-        option2:"Hypertext Markup Language",
-        option3:"HyperText Makeup Language",
-        option4:"Hypertext Markup Literature",
-        answer:"Hypertext Markup Language"
-    }
+  {
+    question:
+      'Which method is used to add elements to the end of an array in JavaScript?',
+    option1: 'array.push()',
+    option2: 'array.unshift()',
+    option3: 'array.append()',
+    option4: 'array.add()',
+    answer: 'array.push()'
+  }
 ]
- var startbutton = document.getElementById("startQuiz")
- startbutton.addEventListener ("click", function(){
-    quizContainer.style.display="block"
-    startbutton.style.display="none"
- })
+var startbutton = document.getElementById('startQuiz')
+startbutton.addEventListener('click', function () {
+  quizContainer.style.display = 'block'
+  displayQuestion()
+  startbutton.style.display = 'none'
+})
+function displayQuestion () {
+  questionElement.innerText = quizArray[currentQuestion].question
+  option1.innerText = quizArray[currentQuestion].option1
+  option2.innerText = quizArray[currentQuestion].option2
+  option3.innerText = quizArray[currentQuestion].option3
+  option4.innerText = quizArray[currentQuestion].option4
+}
+var quizContainer = document.getElementById('quizContainer')
+quizContainer.style.display = 'none'
 
- var quizContainer = document.getElementById("quizContainer")
- quizContainer.style.display="none"
+function correctAnswer (event) {
+  var userSelection = event.target.innerText
+  if (userSelection == quizArray[currentQuestion].answer) {
+    score += 10
+    checkAnswer.innerText="answer is correct"
+  } else { 
+    checkAnswer.innerText="answer is wrong"
+  }
+  if(currentQuestion < quizArray.length-1){
+    currentQuestion++
+    displayQuestion()
+  }else{
+    summary()
+  }
+}
+
+
+function summary(){
+    quizContainer.style.display = 'none'
+}
